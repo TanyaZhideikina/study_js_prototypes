b38b4fea1dc1039bb46f86195b4705ed667775f9
TanyaZhideikina <rotesone1396@gmail.com>
2016-05-24 00:42:56 +0300

    lern prototypes in javasccript

---
+++ learn_js_prototype_1.js
@@ -0,0 +1,35 @@
+////I learn prototypes in Javascript
+
+//// first object - Animal
+function Animal(name){
+    this.name = name;
+    this.speed = 0;
+}
+
+Animal.prototype.run = funtion(speed){
+    this.speed+=speed;
+    alert(this.name+' run and speed '+this.speed);
+};
+
+Animal.prototype.stop = function(){
+    this.speed = 0;
+    alert(this.name+' stand');
+};
+
+
+////second object - Rabbit
+////Animal is prototype for Rabbit
+Rabbit.prototype.__proto__ = Animal.prototype;
+function Rabbit(name){
+    this.name = name;
+    this.speed = 0;    
+}
+
+Rabbit.prototype.jump = function(){
+    this.speed++;
+    alert( this.name + ' jump');
+};
+
+////Rabbit is prototype for rabbit
+var rabbit  = new Rabbit('Bugs bunny');
+
