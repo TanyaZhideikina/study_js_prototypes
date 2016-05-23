8c213a6b718cd0998833a4820af6d12725760f16
TanyaZhideikina <rotesone1396@gmail.com>
2016-05-24 00:52:15 +0300

    learn js prototypes

---
+++ learn_js_prototype_3.js
@@ -0,0 +1,33 @@
+////I learn prototypes in Javascript
+
+//// first object - Animal
+function Animal(name){
+    this.name = name;
+    this.speed = 0;
+}
+
+Animal.prototype.run = funtion(speed){
+    this.speed+=speed;
+    alert(this.name+' run and speed '+this.speed);
+};
+
+Animal.prototype.stop = function(){
+    this.speed = 0;
+    alert(this.name+' stand');
+};
+
+
+////second object Rabbit - new generation
+function Rabbit(name){
+    this.name = name;
+    this.speed = 0;
+}
+////Asking inheritance with constructor method
+Rabbit.prototype = Object.create(Animal.Prototype);
+Rabbit.prototype.constructor = Rabbit;
+
+////and add our method(or methods...)
+Rabbit.prototype.jump = function(){
+    this.speed++;
+    alert( alert( this.name + ' jump, speed ' + this.speed ););
+}
